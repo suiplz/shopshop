ad00bed30eafc62d2c8969b4b6be326acc415c47
suiplz <acepade231@gmail.com>
2022-12-01 17:16:18 +0900

    [Cart Impl] 22/12/01

--- src/main/java/com/example/shopshop/cart/controller/CartApiController.java
+++ src/main/java/com/example/shopshop/cart/controller/CartApiController.java
@@ -1,6 +1,7 @@
 package com.example.shopshop.cart.controller;
 
 import com.example.shopshop.Item.domain.Item;
+import com.example.shopshop.Item.service.ItemService;
 import com.example.shopshop.cart.dto.CartDTO;
 import com.example.shopshop.cart.service.CartService;
 import com.example.shopshop.security.auth.PrincipalDetails;
@@ -8,20 +9,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/cart/api")
+@RequestMapping("/api/cart")
 @RequiredArgsConstructor
 public class CartApiController {
 
     private final CartService cartService;
+    private final ItemService itemService;
 
-    @PostMapping("/register")
-    public ResponseEntity<Long> register(@ModelAttribute CartDTO cartDTO, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+    @PostMapping("/register/{itemId}")
+    public ResponseEntity<Long> register(@PathVariable Long itemId, @ModelAttribute CartDTO cartDTO, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        itemService.getItem(itemId);
         cartDTO.setBuyer(principalDetails.getMember());
         Long register = cartService.register(cartDTO);
 
--- src/main/java/com/example/shopshop/cart/repository/CartItemRepository.java
+++ src/main/java/com/example/shopshop/cart/repository/CartItemRepository.java
@@ -5,4 +5,6 @@ import com.example.shopshop.cart.domain.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    CartItem findByCartIdAndItemId(Long cartId, Long itemId);
 }
